8c7ce1c0b49e9e8d746cb684183e32293ff84153
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-01 23:18:11 -0500

    includes results from PromiseAll

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
 
     this.state = {
       isLoaded: false,
+      johnSnowBorn:'',
       Margaeryplace: '',
       TargaryenRegion:'',
       LannisterCoat:'',
@@ -15,12 +16,14 @@ export default class App extends Component {
       BaratheonSecondAlias:'',
       HouseStarkFounderUrl:'',
       HouseStarkFounder:'',
-      CatelynStarkPOVBooks:''
+      CatelynStarkPOVBooksUrls:'',
+      CatelynStarkPOVBooks:[]
 
     }
   }
 
   componentDidMount() {
+    let zero="https://anapioficeandfire.com/api/characters/583"
     let one = "https://anapioficeandfire.com/api/characters/16"
     let two = "https://www.anapioficeandfire.com/api/houses/378"
     let three = "https://www.anapioficeandfire.com/api/houses/229"
@@ -28,70 +31,57 @@ export default class App extends Component {
     let five = "https://www.anapioficeandfire.com/api/characters/901"
     let six = "https://www.anapioficeandfire.com/api/houses/362"
     let seven = "https://www.anapioficeandfire.com/api/characters/232"
-    const requestOne = axios(one, {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
+    
+    // axios.proxy={
+      
+    //     host: '127.0.0.1',
+    //     port: 3000
+      
+    // }
+     axios.headers={
+ 
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
-      }
+     }
+    const requestZero = axios(zero, {
+      method: 'GET'
+    })
+   
+    const requestOne = axios(one, {
+      method: 'GET'
     })
     const requestTwo = axios(two, {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      }
+      method: 'GET'
+      
     })
     const requestThree = axios(three, {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      }
+      method: 'GET'
     })
     const requestFour = axios(four, {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      }
+      method: 'GET'
+      
     })
     const requestFive = axios(five, {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      }
+      method: 'GET'
     })
     const requestSix = axios(six, {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      }
+      method: 'GET'
     })
     const requestSeven = axios(seven, {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      }
+      method: 'GET'
     })
-    axios.all([requestOne, requestTwo, requestThree,requestFour,requestFive,requestSix,requestSeven]).then(axios.spread((...responses) => {
-       const responseOne = responses[0]
-       const responseTwo = responses[1]
-       const responesThree = responses[2]
-       const responseFour = responses[3]
-       const responseFive = responses[4]
-       const responesSix = responses[5]
-       const responesSeven = responses[6]
+    // console.log("request is");
+    // console.log(axios.request);
+    axios.all([requestZero,requestOne, requestTwo, requestThree,requestFour,requestFive,requestSix,requestSeven]).then(axios.spread((...responses) => {
+      const responseZero = responses[1]
+      const responseOne = responses[1]
+       const responseTwo = responses[2]
+       const responesThree = responses[3]
+       const responseFour = responses[4]
+       const responseFive = responses[5]
+       const responesSix = responses[6]
+       const responesSeven = responses[7]
+       this.setState({johnSnowBorn:responseZero.data.born})
         this.setState({Margaeryplace:responseOne.data.born})
         this.setState({TargaryenRegion:responseTwo.data.region})
         this.setState({LannisterCoat:responesThree.data.coatOfArms})
@@ -101,19 +91,45 @@ export default class App extends Component {
         let url=this.state.HouseStarkFounderUrl;
         const requestEight = axios(url, {
           method: 'GET',
-          mode: 'no-cors',
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-          }
+          // mode: 'no-cors',
+          //  headers: {
+          //    'Access-Control-Allow-Origin': 'http://localhost:3000',
+          //    'Content-Type': 'application/json',
+          //  }
+          // proxy: {
+          //   host: '127.0.0.1',
+          //   port: 9000
+          // }
         }).then(res => {          
           this.setState({HouseStarkFounder: res.data.name})
+          // console.log("response is");
+          // console.log(res)
     
         })
         .catch(error => {
           console.log('there is an eror', error)
         })
-        this.setState({CatelynStarkPOVBooks:responesSeven.data.povBooks})
+        this.setState({CatelynStarkPOVBooksUrls:responesSeven.data.povBooks})
+        let urls=this.state.CatelynStarkPOVBooksUrls;
+        console.log("urls");
+        console.log(urls);
+        let getRequest=url=>{
+          console.log("inside getRequest");
+          console.log(url)
+          return axios(url, {
+            method: 'GET'});
+        }
+        let Promises =urls.map(url=>getRequest(url));
+          const getBooks = list=>{
+            this.setState({CatelynStarkPOVBooks: list})
+            // console.log(typeof this.state.CatelynStarkPOVBooks)
+          }
+         Promise.all(Promises).then(values=>getBooks(values)).catch(errors => {
+             // react on errors.
+          })
+      
+        // });
+
       // use/access the results 
 
     })).catch(errors => {
@@ -122,9 +138,12 @@ export default class App extends Component {
 
   }
   render() {
-    return (
+   console.log("inside render")
+  let items = this.state.CatelynStarkPOVBooks.map((item,id)=><li key={id}>{item.data.name}</li>)
+   return (
       <div>
-
+        <h1>When was Jon Snow born?</h1>
+        {this.state.johnSnowBorn}
        <h1>Where was Margaery Tyrell born?</h1>
        {this.state.Margaeryplace}
 
@@ -140,6 +159,7 @@ export default class App extends Component {
         {this.state.HouseStarkFounder}
         <h1>What are the titles of Catelyn Stark's three POV books? (Look into Promise.all to request these simultaniously)</h1>
         <ul>
+          {items}
         </ul>
       </div>
     )
